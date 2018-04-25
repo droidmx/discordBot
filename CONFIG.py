bd876fb4837b2876aab13634efe44166f25b0c77
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-04-25 10:11:44 -0400

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,3 +1,3 @@
-TOKEN = ""   # Discord Bot Token
-PREFIX = "+"    # Command Prefix; example: +ping
-GAME = ""     # enter what should be shown after "Playing ..." below the Discord Username
+TOKEN = "NDM4NjkyMTc5MDU0MDM0OTQ3.DcIVSw.F2cgutZX41mfsMh2qcxVg7LuUOk"   # Discord Bot Token
+PREFIX = "!"    # Command Prefix; example: +ping
+GAME = "Realm of the Mad God"     # enter what should be shown after "Playing ..." below the Discord Username
